de0c7a5e44347af914671fb1ccf2338f94a4e302
Ben Miller <benjaminmiller229@gmail.com>
2016-04-05 23:41:46 -0400

    Create main.java

---
+++ main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String x = "aaabcdefg";
+		char y = x.charAt(3);
+		
+		System.out.println(y);
+	}
+}
